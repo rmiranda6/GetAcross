9581b7e85c71795d8daacfbc73109582e9b71303
RickyBobby <55592577+rmiranda6@users.noreply.github.com>
2019-10-28 00:02:51 -0500

    Added a lot of changes and made a lot of progess

---
+++ GetAcross/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    Rigidbody rb;
+    moveForward moveForward;
+    BoxCollider boxCollider;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        moveForward = GetComponent<moveForward>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+
+            Destroy(gameObject, 2f);
+
+            Vector3 direction = Quaternion.Euler(-20, 0, 0) * -transform.forward;
+            rb.AddForce(direction * 20f, ForceMode.Impulse);
+            rb.AddTorque(Random.insideUnitSphere * 15f, ForceMode.Impulse);
+
+            moveForward.enabled = false;
+            boxCollider.enabled = false;
+
+            Destroy(other.gameObject);
+        }
+
+    }
+}
+
---
+++ GetAcross/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] animalPrefabs;
+    [SerializeField] float xSpawnRangeClose = 103;
+    [SerializeField] float xSpawnRangeFar = 3;
+    [SerializeField] float zSpawnPosition = -28;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnAnimal", 1.5f, 1f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S)) SpawnAnimal();
+    }
+
+    void SpawnAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        GameObject animalPrefab = animalPrefabs[animalIndex];
+        Vector3 pos = new Vector3(Random.Range(xSpawnRangeClose, xSpawnRangeFar), 0, zSpawnPosition);
+        Instantiate(animalPrefab, pos, animalPrefab.transform.rotation);
+    }
+}
